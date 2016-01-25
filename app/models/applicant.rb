f8e5270834c82faa1dcf8c40fa95a4be672b1a0a
Matt Herich <mjherich@gmail.com>
2016-01-25 22:04:14 +0000

    Add nested attributes to applicant model

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,4 +2,6 @@ class Applicant < ActiveRecord::Base
   has_many :previous_employments, dependent: :destroy
   has_many :references, dependent: :destroy
   has_one :personal_transportation_info, dependent: :destroy
+  
+  accepts_nested_attributes_for :previous_employments, :references, :personal_transportation_info
 end
