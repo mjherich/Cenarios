a5641dce2e9e1dbdc8506dbfa7594435c88809ec
Matt Herich <mjherich@gmail.com>
2016-03-04 00:34:31 +0000

    Begin population Employees controller new/create actions and params

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,17 @@
 class EmployeesController < ApplicationController
   def new
+    @employee = Employee.new
   end
 
   def create
+    @employee = Employee.create(employee_params)
+    
+    if @employee.save
+    end
   end
+  
+  private
+    def employee_params
+      params.require(:employee).permit(:name, :email, :comment) # Modify this for all Employee instance params
+    end
 end
