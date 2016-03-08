ee4e3f8305e0cef7ea4c96ed7b2f432dc21f0806
Matt Herich <mjherich@gmail.com>
2016-03-08 22:56:16 +0000

    Disable simple form country mapping

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -51,6 +51,9 @@ SimpleForm.setup do |config|
     # b.use :full_error, wrap_with: { tag: :span, class: :error }
   end
 
+  # Disable country helper
+  config.input_mappings = { /country/ => :string }
+
   # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :default
 
